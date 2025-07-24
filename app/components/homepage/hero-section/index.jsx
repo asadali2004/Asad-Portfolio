af8d3f2837d81e508a265fb5e6e11ca4630d7b95
Asad <aliasad07518@gmail.com>
2025-07-24 22:33:24 +0530

    home page updation

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -150,8 +150,6 @@ function HeroSection() {
                   <span className="text-gray-400">{`['`}</span>
                   <span className="text-amber-300">JavaScript</span>
                   <span className="text-gray-400">{"', '"}</span>
-                  <span className="text-amber-300">Python</span>
-                  <span className="text-gray-400">{"', '"}</span>
                   <span className="text-amber-300">Java</span>
                   <span className="text-gray-400">{"', '"}</span>
                   <span className="text-amber-300">React</span>
